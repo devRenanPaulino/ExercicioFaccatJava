3c81e9c2ad10abbfa970456deea03ab58ffa2882
Renan Paulino <dev.renanpaulino@gmail.com>
2025-06-06 14:25:54 -0300

    Update DoWhile.java
    correção da tarefa

--- Equipe4/src/equipe04/DoWhile.java
+++ Equipe4/src/equipe04/DoWhile.java
@@ -2,11 +2,11 @@ package equipe04;
 
 public class DoWhile {
 	public static void main(String[] args) {
-		int i = 0; // declara e atribui 
-		while(i < 5); // condição
-		{
-			System.out.println(i); //saída
-			i++; // incrementa 
-		}
+		int i = 1; // declara e atribui 
+		
+		do {
+			System.out.println("Contador: " + i + "Â°");
+			i++;
+		} while(i<6);
 	}
 }
